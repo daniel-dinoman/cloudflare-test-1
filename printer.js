ec137da1c5716ede4e9c162911128032c0a92212
daniel-dinoman <danielsmeyersjr@gmail.com>
2026-09-18 14:18:03 -0400

    Update and rename printer.js to index.html

--- printer.js
+++
@@ -1,5 +0,0 @@
-export default {
-  async fetch(request) {
-    return new Response("Welcome to the orange peel club! Current members: daniel_dinoman, noob_ncp101, error_40444, and neville. This site is still being created, so come back tomorrow for updates.");
-  }
-};
